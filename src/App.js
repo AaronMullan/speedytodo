4c343a6efaf1746824db978c104371ddba691588
AaronMullan <aaroncmullan@gmail.com>
2020-06-04 14:11:44 -0700

    eslinting my life away

--- src/App.js
+++ src/App.js
@@ -1,53 +1,44 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState(['do it', 'f yeah', 'oh buddy']);
-  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState(["do it", "f yeah", "oh buddy"]);
+  const [value, setValue] = useState("");
 
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  const addTodo = (todo) => {
+    const newTodos = [...todos, todo];
+    setTodos(newTodos);
+  };
   const handleChange = (e) => {
     setValue(e.target.value);
-    console.log(value)
-  }
-  const handleSubmit = e => {
-    e.preventDefault()
-    addTodo(value)
-  }
-  const deleteTodo = index => {
-    const newTodos = [...todos]
-    newTodos.splice(index, 1)
-    setTodos(newTodos);
-  }
-  const addTodo = (todo) => {
-    const newTodos = [...todos, todo]
-    setTodos(newTodos);
-  }
+    console.log(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+  };
 
   return (
     <div className="App">
-     <ul>
-       {todos.map((element, index) => 
-         <li 
-         key={index}>
-           {element}
-           <button
-           onClick={() => deleteTodo(index)}>
-             delete</button>
+      <ul>
+        {todos.map((element, index) => (
+          <li key={index}>
+            {element}
+            <button type="button" onClick={() => deleteTodo(index)}>
+              delete
+            </button>
           </li>
-       )}
-     </ul>
-     <form onSubmit={handleSubmit}>
-       <input 
-       type="text"
-       value={value}
-       onChange={handleChange}
-       />
-       <input 
-       type="submit"
-       value="Submit"
-       
-       />
-     </form>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
